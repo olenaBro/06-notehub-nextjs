0e8c8e2bc628bb68a7eedb60492ca286d95b71d7
Olena <olenkazaria@gmail.com>
2026-04-13 16:32:50 +0200

    Add refetchOnMount false to NoteDetails

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -9,14 +9,15 @@ export default function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
 
   const {
-    data: note,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['note', id],
-    queryFn: () => fetchNoteById(id),
-    enabled: !!id,
-  });
+  data: note,
+  isLoading,
+  error,
+} = useQuery({
+  queryKey: ['note', id],
+  queryFn: () => fetchNoteById(id),
+  enabled: !!id,
+  refetchOnMount: false,
+});
 
   if (isLoading) {
     return <p>Loading, please wait...</p>;
